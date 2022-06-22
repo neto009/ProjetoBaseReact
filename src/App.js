68e1d0c86e706f667329379fe34ef1734b1dff85
aluno <aluno@udicentro.local>
2022-06-22 20:10:36 -0300

    Ajuste

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
 
 export default class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div>
+          <Header/>
+          <Main/>
+          <Footer/>
+      </div>
     )
   }
 }
---
+++ src/Menu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './css/style.css';
+
+export default class Menu extends Component {
+  render() {
+    return (
+      <nav >
+          <a href="#">Home</a>
+          <a href="#">Sobre nós</a>
+          <a href="#">Contato</a>
+      </nav>
+    )
+  }
+}
